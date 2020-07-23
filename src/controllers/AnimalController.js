f0d208bb4192dd54cd255e1336434c806f098d44
Joedyson Bezerra <joedysonbezerra@gmail.com>
2020-07-23 00:29:46 -0300

    feat(store): ensure AnimalController return an error 400 if no type is provided

--- src/controllers/AnimalController.js
+++ src/controllers/AnimalController.js
@@ -3,7 +3,7 @@ import { MissingParamError } from '../utils/errors/MissingParamError';
 class AnimalController {
   async store(request, response) {
     const { body } = request;
-    const requiredFields = ['name', 'age'];
+    const requiredFields = ['name', 'age', 'type'];
 
     for (const field of requiredFields) {
       if (!body[field]) throw new MissingParamError(field);
