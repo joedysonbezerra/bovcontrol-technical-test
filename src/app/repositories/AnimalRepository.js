4b547ff9387e471ccb02ec2bd190401081bf9eb7
Joedyson Bezerra <joedysonbezerra@gmail.com>
2020-07-23 16:18:46 -0300

    feat: create AnimalRepositoryMongo

---
+++ src/app/repositories/AnimalRepository.js
@@ -0,0 +1,22 @@
+import Animals from '../models/animals';
+
+class AnimalRepositoryMongo {
+  async findOne(id) {
+    const animal = await Animals.findById(id);
+    return animal;
+  }
+
+  async update(id, body) {
+    const animal = await Animals.findByIdAndUpdate(id, body, {
+      new: true,
+    });
+    return animal;
+  }
+
+  async add({ name, age, type, weight }) {
+    const animal = await Animals.create({ name, age, type, weight });
+    return animal;
+  }
+}
+
+export default AnimalRepositoryMongo;
