919029f468cdfad63ac2a4e9be4680fdf93ca6a3
Joedyson Bezerra <joedysonbezerra@gmail.com>
2020-07-23 02:25:22 -0300

    feat: ensure CreateAnimalService return 400 if an invalid weight is provided

--- src/services/CreateAnimalService.js
+++ src/services/CreateAnimalService.js
@@ -3,6 +3,7 @@ import { InvalidParamError } from '../utils/errors/InvalidParamError';
 class CreateAnimalService {
   async execute({ name, age, type, weight }) {
     if (age <= 0) throw new InvalidParamError('age');
+    if (weight <= 0) throw new InvalidParamError('weight');
   }
 }
 
