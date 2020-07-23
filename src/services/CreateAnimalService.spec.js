c2ef60443488bea17cee78f078b220aeb172ebdb
Joedyson Bezerra <joedysonbezerra@gmail.com>
2020-07-23 02:25:35 -0300

    test: ensure CreateAnimalService return 400 if an invalid weight is provided

--- src/services/CreateAnimalService.spec.js
+++ src/services/CreateAnimalService.spec.js
@@ -24,4 +24,22 @@ describe('CreateAnimalService', () => {
       expect(error).toEqual(new InvalidParamError('age'));
     }
   });
+  it('Should be able return 400 if an invalid weight is provided', async () => {
+    const sut = makeSut();
+
+    const httpRequest = {
+      body: {
+        name: 'any_name',
+        age: 10,
+        type: 'cow',
+        weight: -1,
+      },
+    };
+    try {
+      await sut.execute(httpRequest);
+    } catch (error) {
+      expect(error.statusCode).toBe(400);
+      expect(error).toEqual(new InvalidParamError('weight'));
+    }
+  });
 });
