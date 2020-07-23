d6b4039e573000f2e75c7ff31f1e896117b6e9e3
Joedyson Bezerra <joedysonbezerra@gmail.com>
2020-07-23 02:22:27 -0300

    test: ensure CreateAnimalService return 400 if an invalid age is provided

---
+++ src/services/CreateAnimalService.spec.js
@@ -0,0 +1,27 @@
+import CreateAnimalService from './CreateAnimalService';
+import { InvalidParamError } from '../utils/errors/InvalidParamError';
+
+const makeSut = () => {
+  return new CreateAnimalService();
+};
+
+describe('CreateAnimalService', () => {
+  it('Should be able return 400 if an invalid age is provided', async () => {
+    const sut = makeSut();
+
+    const httpRequest = {
+      body: {
+        name: 'any_name',
+        age: -1,
+        type: 'cow',
+        weight: 100,
+      },
+    };
+    try {
+      await sut.execute(httpRequest);
+    } catch (error) {
+      expect(error.statusCode).toBe(400);
+      expect(error).toEqual(new InvalidParamError('age'));
+    }
+  });
+});
