18a2bb3ee5c774753a3db3c277c98c9225093e2f
Joedyson Bezerra <joedysonbezerra@gmail.com>
2020-07-23 18:06:43 -0300

    test: ensure endpoint (get) find the animal in collection of mongodb

--- src/routes/animals.routes.test.js
+++ src/routes/animals.routes.test.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-underscore-dangle */
 import request from 'supertest';
 import { connect, disconnect } from 'mongoose';
 
@@ -26,5 +27,21 @@ describe('Animals - Integration', () => {
     });
     expect(response.status).toBe(200);
     expect(response.body).toHaveProperty('_id');
+  });
+  it('Should be able find the animal in collection of mongodb', async () => {
+    const animal = await request(app).post('/animals').send({
+      name: 'any_name',
+      dateOfBirth: '2014-07-02T03:00:00.000Z',
+      type: 'cow',
+      weight: 100,
+    });
+    const response = await request(app).get(`/animals/${animal.body._id}`);
+
+    expect(response.status).toBe(200);
+    expect(response.body._id).toBe(animal.body._id);
+    expect(response.body.name).toBe(animal.body.name);
+    expect(response.body.age).toBe(animal.body.age);
+    expect(response.body.type).toBe(animal.body.type);
+    expect(response.body.weight).toBe(animal.body.weight);
   });
 });
