063ad6139ad0bee338fe8dd1cb821248d93178be
Joedyson Bezerra <joedysonbezerra@gmail.com>
2020-07-23 02:48:40 -0300

    feat: create a new animal

---
+++ src/entities/Animal.js
@@ -0,0 +1,10 @@
+class Animal {
+  constructor(name, age, type, weight) {
+    this.name = name;
+    this.age = age;
+    this.type = type;
+    this.weight = weight;
+  }
+}
+
+export default Animal;
--- src/services/CreateAnimalService.js
+++ src/services/CreateAnimalService.js
@@ -1,9 +1,17 @@
 import { InvalidParamError } from '../utils/errors/InvalidParamError';
+import Animal from '../entities/Animal';
 
 class CreateAnimalService {
+  constructor(AnimalRepository) {
+    this.AnimalRepository = AnimalRepository;
+  }
+
   async execute({ name, age, type, weight }) {
     if (age <= 0) throw new InvalidParamError('age');
+
     if (weight <= 0) throw new InvalidParamError('weight');
+
+    return this.AnimalRepository.add(new Animal(name, age, type, weight));
   }
 }
 
