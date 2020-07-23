f9b3173a97f95734320df8ae3f6ac31b90769ccb
Joedyson Bezerra <joedysonbezerra@gmail.com>
2020-07-23 00:34:26 -0300

    feat(store): ensure AnimalController return an error 400 if no weight is provided

--- src/controllers/AnimalController.js
+++ src/controllers/AnimalController.js
@@ -3,7 +3,7 @@ import { MissingParamError } from '../utils/errors/MissingParamError';
 class AnimalController {
   async store(request, response) {
     const { body } = request;
-    const requiredFields = ['name', 'age', 'type'];
+    const requiredFields = ['name', 'age', 'type', 'weight'];
 
     for (const field of requiredFields) {
       if (!body[field]) throw new MissingParamError(field);
